c9e143d28de23f57b627b79c3719b929e5a0bd8e
emaphis <emaphis85@gmail.com>
2022-02-11 11:37:51 -0500

    Updates

---
+++ src/main/java/my/project/java/loom/JavaLoomApp.java
@@ -0,0 +1,12 @@
+
+package my.project.java.loom;
+
+/**
+ *
+ * @author emaph
+ */
+public class JavaLoomApp {
+    public static void main(String[] args) {
+        System.out.println("I'm a Loom App");
+    }
+}
