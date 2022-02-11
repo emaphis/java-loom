925e8b637ead6bad9381080a7cdf5610a0e4ad0a
emaphis <emaphis85@gmail.com>
2022-02-11 11:32:43 -0500

    Update

---
+++ src/main/java/my/project/java/loom/JavaLoomVersion.java
@@ -0,0 +1,33 @@
+
+package my.project.java.loom;
+
+import java.util.Properties;
+
+/**
+ *
+ * @author emaph
+ */
+public class JavaLoomVersion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Loom!\n");System.out.println("******************** Hello, JDK ********************");
+        Properties prop = System.getProperties();
+
+        String version = prop.getProperty("java.version");
+        String vendor = prop.getProperty("java.vendor");
+        String date = prop.getProperty("java.version.date");
+        System.out.println("Vendor:    " + vendor + " : " + version + " : " + date);
+
+        String vm = prop.getProperty("java.vm.name");
+        String vmversion = prop.getProperty("java.vm.version");
+        System.out.println("Java VM:   " + vm + " : " + vmversion);
+
+        String home = prop.getProperty("java.home");
+        String path = prop.getProperty("user.dir");
+        String temp = prop.getProperty("java.io.tmpdir");
+        System.out.println("Java home: " + home);
+        System.out.println("User dir:  " + path);
+        System.out.println("Temp dir:  " + temp);
+        System.out.println("****************************************************");
+    }
+}
