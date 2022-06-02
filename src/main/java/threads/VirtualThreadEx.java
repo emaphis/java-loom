472dd67815ac260563231adba3b5f48a6fc2f1bc
emaphis <emaphis85@gmail.com>
2022-06-02 18:26:54 -0400

    Tinkeing

--- src/main/java/threads/VirtualThreadEx.java
+++ src/main/java/threads/VirtualThreadEx.java
@@ -7,15 +7,9 @@ import java.util.stream.IntStream;
 
 public class VirtualThreadEx {
     public static void main(String[] args) {
-
-        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            IntStream.range(0, 100).forEach(i -> {
-                executor.submit(() -> {
-                    Thread.sleep(Duration.ofSeconds(1));
-                    return i;
-                });
-            });
-        }
+        Thread.startVirtualThread(() ->
+           System.out.println("Hello Thread")
+        );
 
     }
 }
